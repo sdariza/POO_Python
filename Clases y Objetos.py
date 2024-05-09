3f3560ccd61ac4245378057f006be6b0f6bc9faa
sdariza <sdariza@uninorte.edu.co>
2024-05-09 17:55:50 -0500

    feat: ✨add Clases y Objetos: creación de objetos con atributos estáticos

---
+++ Clases y Objetos.py
@@ -0,0 +1,17 @@
+# class NombreClase():
+#     propiedad1 = "valor 1"
+#     propiedad2 = "valor 2"
+#     propiedad3 = "valor 3"
+
+class Celular():
+
+    marca = "Samsung"
+    modelo = "s23"
+    camara = "48MP"
+
+
+if __name__ == "__main__":
+    celular1 = Celular()
+    celular2 = Celular()
+    celular3 = Celular()
+    print(celular3.camara)
